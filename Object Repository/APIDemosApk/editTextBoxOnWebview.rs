e70e065d6d4e75a3158d15c80b158f59cce84fc1
Saurabh <saurabh.d2106@gmail.com>
2021-12-02 17:08:09 +0530

    Day 4 code

---
+++ Object Repository/APIDemosApk/editTextBoxOnWebview.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>editTextBoxOnWebview</name>
+   <tag></tag>
+   <elementGuidId>1094a987-c4a1-48dd-8109-4f0975e3a11c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'i_am_a_textbox']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>i_am_a_textbox</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Scripts/APIDemos/WebViewTesting/Script1638431415639.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.MobileTestObject
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.testobject.MobileTestObject.MobileLocatorStrategy
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+
+Mobile.tap(findTestObject('Object Repository/APIDemosApk/viewsLink') , GlobalVariable.elementDetectionTimeout)
+	
+Mobile.scrollToText("WebView")
+
+MobileTestObject webView = findTestObject('Object Repository/APIDemosApk/webViewLink')
+
+webView.setMobileLocatorStrategy(MobileLocatorStrategy.ACCESSIBILITY)
+
+webView.setMobileLocator("WebView")
+
+
+Mobile.tap(webView, GlobalVariable.elementDetectionTimeout)
+
+Mobile.switchToWebView()
+
+WebUI.sendKeys(findTestObject('Object Repository/APIDemosApk/editTextBoxOnWebview'), "Saurabh Dhingra")
+
+
+
+
+
